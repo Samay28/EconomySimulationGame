2a6bbc99fa424eb2c7db3c74fb1657646120ba0e
Samay28 <imsamaystar@gmail.com>
2024-06-20 22:22:07 +0530

    setting up vegetable shop

--- Source/EconomySimulation/Public/VegetableShop.h
+++ Source/EconomySimulation/Public/VegetableShop.h
@@ -1,20 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "MyPlayerCharacter.h"
-#include "OresShop.generated.h"
+#include "VegetableShop.generated.h"
 
 UCLASS()
-class ECONOMYSIMULATION_API AOresShop : public AActor
+class ECONOMYSIMULATION_API AVegetableShop : public AActor
 {
 	GENERATED_BODY()
 
 public:
-	AOresShop();
-
-	virtual void Tick(float DeltaTime) override;
+	AVegetableShop();
 
 	UFUNCTION(BlueprintCallable, Category = "Shop")
 	void SellItem(AMyPlayerCharacter *PlayerCharacter);
@@ -23,7 +22,7 @@ public:
 	UStaticMeshComponent *ShopMesh;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 	class AGameManager *GM;
 };
