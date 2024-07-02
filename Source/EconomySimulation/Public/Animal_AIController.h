b71a08eebcdf6c7b5c8b14d28ac6b5aa41358e61
Samay28 <imsamaystar@gmail.com>
2024-07-02 10:13:29 +0530

    cow animation basic

---
+++ Source/EconomySimulation/Public/Animal_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Animal_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECONOMYSIMULATION_API AAnimal_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
